c00f66c59725794390a7a4694e5f6e02ca20ccee
DeWitt Kane <dewitt.kane@gmail.com>
2022-02-01 10:57:38 -0500

    Add sidebar

--- src/components/Details.js
+++ src/components/Details.js
@@ -3,13 +3,26 @@ import PropTypes from 'prop-types';
 import { itemImages } from '../items';
 import './Details.css';
 import NotFound from './NotFound';
+import Thumbnail from './Thumbnail';
 
 function Details({ items }) {
   const { id } = useParams();
   const detailItem = items.find((item) => item.id === id);
+  const otherItems = items.filter((item) => item.id !== id);
 
   return (
     <div className="details-component">
+      <div className="details-sidebar">
+        <h2>Other Items</h2>
+        {otherItems.map((item) => (
+          <Thumbnail
+            id={item.id}
+            image={itemImages[item.imageId]}
+            title={item.title}
+            key={item.id}
+          />
+        ))}
+      </div>
       <div className="details-box">
         { detailItem ? (
           <>
